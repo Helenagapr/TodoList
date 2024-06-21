80e312facbb4bc06ed8bbcba2f45580a53d84eed
Helena <helenagp05@gmail.com>
2024-06-21 11:15:57 +0200

    canvis

--- TodoList.App/obj/Debug/net8.0/TodoList.App.AssemblyInfo.cs
+++ TodoList.App/obj/Debug/net8.0/TodoList.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3b2d3a58c973dca4988df065701a75cdd9460c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d069c939c2b0e110572c26fd71f9b5ca611bad4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
